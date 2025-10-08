9dc19fe14c9c989bedd7333a1c5ed41b271ce6b7
Tung <kristensendo.26@gmail.com>
2025-10-08 15:43:16 +0700

    Update trường aupdate author Joi

--- src/validation/author.validation.js
+++ src/validation/author.validation.js
@@ -3,14 +3,16 @@ import Joi from "joi";
 // Schema tạo tác giả mới
 
 export const createAuthorSchema = Joi.object({
-    name: Joi.string().required().max(200).messages({
+    name: Joi.string().required().min(2).max(100).messages({
         "string.base": "Tên tác giả phải là chuỗi",
         "string.emty": "Tên tác giả không được để trống",
         "string.max": "Tên sản phẩm không được vượt quá {#limit} ký tự",
+        "string.min": "Tên tác giả phải có ít nhất {#limit} ký tự",
         "any.required": "Tên tác giả là bắt buộc",
     }),
-    bio: Joi.string().optional().messages({
+    bio: Joi.string().optional().max(500).messages({
         "string.base": "Tiểu sử tác giả phải là chuỗi",
+        "string.max": "Tiểu sử không được vượt quá {#limit} ký tự",
     }),
     createdAt: Joi.date().default(()=> new Date()),
     // Tự gán ngày hiện tại
@@ -19,10 +21,18 @@ export const createAuthorSchema = Joi.object({
 });
 
 // Schema cập nhật tác giả
-export const updateAuthorSchema = createAuthorSchema.fork(
-    ["name", "bio"],
-    (schema) => schema.optional()
-);
+export const updateAuthorSchema = Joi.object({
+    name: Joi.string().min(2).max(100).optional().messages({
+        "string.base": "Tên tác giả phải là chuỗi",
+        "string.emty": "Tên tác giả không được để trống",
+        "string.max": "Tên sản phẩm không được vượt quá {#limit} ký tự",
+        "string.min": "Tên tác giả phải có ít nhất {#limit} ký tự",
+    }),
+    bio: Joi.string().max(500).optional().messages({
+        "string.base": "Tiểu sử tác giả phải là chuỗi",
+        "string.max": "Tiểu sử không được vượt quá {#limit} ký tự",
+    }),
+});
 
 // fork: Hàm của Joi cho phép tùy chỉnh lại các field cụ thể của schema hiện có
 // schema.fork(keys, adjuster)
